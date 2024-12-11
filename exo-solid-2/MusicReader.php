3a0ebc87ddaa8d0772ca90cc6e570e373a7cf840
Enzo VANDEPOELE <ev399356@iut21.u-bourgogne.fr>
2024-12-11 10:57:45 +0100

    O as Open/Closed Principle

---
+++ exo-solid-2/MusicReader.php
@@ -0,0 +1,16 @@
+<?php
+
+class MusicReader
+{
+    private $musicType;
+
+    public function __construct(MusicType $musicType)
+    {
+        $this->musicType = $musicType;
+    }
+
+    public function listen()
+    {
+        return $this->musicType->listen();
+    }
+}
